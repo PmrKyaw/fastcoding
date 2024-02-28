996f092cd256ab46fcb4d790305f206878088f55
Pmr Kyaw <100857567+PmrKyaw@users.noreply.github.com>
2024-02-28 15:25:15 +0630

    Add files via upload

--- public/custom/animation.js
+++ public/custom/animation.js
@@ -47,33 +47,5 @@ document
 
     document.querySelector('.ta-nav-mb').classList.toggle('active');
     document.querySelector('.ta-nav').classList.toggle('active');
+    document.body.classList.toggle('disable-scroll');
   });
-
-// CUSTOM CODE FOR STICKY IN/OUT NAVBAR ANIMATION
-var lastScrollTop = 0;
-
-document.addEventListener(
-  'scroll',
-  function () {
-    var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-    if (st > lastScrollTop) {
-      gsap.to('.navbar-main', {
-        transform:
-          'translate3d(0px, -100%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-        duration: 0.5,
-        ease: 'easeIn',
-      });
-      // downscroll code
-    } else if (st < lastScrollTop) {
-      gsap.to('.navbar-main', {
-        transform:
-          'translate3d(0px, 0%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-        duration: 0.5,
-        ease: 'easeIn',
-      });
-      // upscroll code
-    }
-    lastScrollTop = st <= 0 ? 0 : st;
-  },
-  false
-);
